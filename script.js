9549825288fcc0d2d62c2f152af4d2e1af978077
Jordan Weitz <jordan.weitz@udemy.com>
2015-09-14 17:27:19 -0700

    Added support for modules with protocols

--- script.js
+++ script.js
@@ -1,46 +1,55 @@
+/*
+ * https://www.npmjs.com/package/system-script
+ * system.js plugin to load an import via a script tag instead of a XHR request which avoid CORS issues
+ * */
+'use strict';
+exports.locate = function(load) {
+    var name = load.name.replace(/^(https?:)?\/\//, '');
+    return window.location.protocol + '//' + name;
+};
 exports.locate = function(load) {
   return window.location.protocol + '//' + load.name;
 };
 
 exports.fetch = function(load) {
-  return new Promise(function(resolve, reject) {
-    var head = document.getElementsByTagName('head')[0];
-    var s = document.createElement('script');
-    s.async = true;
+    return new Promise(function(resolve, reject) {
+        var head = document.getElementsByTagName('head')[0];
+        var s = document.createElement('script');
+        s.async = true;
 
-    function complete(evt) {
-      if (s.readyState && s.readyState !== 'loaded' && s.readyState != 'complete'){
-        return;
-      }
-      cleanup();
-      resolve('');
-    }
+        function complete(/*evt*/) {
+            if (s.readyState && s.readyState !== 'loaded' && s.readyState !== 'complete'){
+                return;
+            }
+            cleanup();
+            resolve('');
+        }
 
-    function error(evt) {
-      cleanup();
-      reject(evt);
-    }
+        function error(evt) {
+            cleanup();
+            reject(evt);
+        }
 
-    if (s.attachEvent) {
-      s.attachEvent('onreadystatechange', complete);
-    }
-    else {
-      s.addEventListener('load', complete, false);
-      s.addEventListener('error', error, false);
-    }
+        if (s.attachEvent) {
+            s.attachEvent('onreadystatechange', complete);
+        }
+        else {
+            s.addEventListener('load', complete, false);
+            s.addEventListener('error', error, false);
+        }
 
-    s.src = load.address;
-    head.appendChild(s);
+        s.src = load.address;
+        head.appendChild(s);
 
-    function cleanup() {
-      if (s.detachEvent){
-        s.detachEvent('onreadystatechange', complete);
-      }
-      else {
-        s.removeEventListener('load', complete, false);
-        s.removeEventListener('error', error, false);
-      }
-      head.removeChild(s);
-    }
-  });
+        function cleanup() {
+            if (s.detachEvent){
+                s.detachEvent('onreadystatechange', complete);
+            }
+            else {
+                s.removeEventListener('load', complete, false);
+                s.removeEventListener('error', error, false);
+            }
+            head.removeChild(s);
+        }
+    });
 };
